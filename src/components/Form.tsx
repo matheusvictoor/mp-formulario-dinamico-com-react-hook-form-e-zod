75a7e4c3fbd0ebb343316695570e15a3a6e2e07d
Matheus Victor <matheusvictor.dev@gmail.com>
2025-01-13 11:18:49 -0300

    feat: formulario com o zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,58 +1,193 @@
-import { EyeIcon } from 'lucide-react';
-// import { EyeOffIcon } from 'lucide-react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeIcon, EyeOffIcon, Loader2 } from "lucide-react";
+import React, { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { useHookFormMask } from "use-mask-input";
+import { UserRegister, userRegisterSchema } from "../schema";
+import toast from "react-hot-toast";
 
 export default function Form() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    setError,
+    reset,
+    formState: { isSubmitting, errors },
+  } = useForm<UserRegister>({ resolver: zodResolver(userRegisterSchema) });
+  const registerWithMask = useHookFormMask(register);
+
+  async function handleZipcodeBlur(e: React.FocusEvent<HTMLInputElement>) {
+    const zipcode = e.target.value;
+    const res = await fetch(`https://brasilapi.com.br/api/cep/v2/${zipcode}`);
+
+    if (res.ok) {
+      const data = await res.json();
+      setValue("address", data.street);
+      setValue("city", data.city);
+    }
+  }
+
+  async function onSubmit(data: FieldValues) {
+    console.log(data);
+
+    const res = await fetch(
+      "https://apis.codante.io/api/register-user/register",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    const resData = await res.json();
+
+    if (!res.ok) {
+      for (const field in resData.errors) {
+        setError(field as keyof UserRegister, { type: "manual", message: resData.errors[field] });
+      }
+      toast.error("Erro ao cadastrar usuário")
+    } else {
+      console.log(resData);
+      toast.success("Usuário cadastrado com sucesso")
+      reset();
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4">
         <label htmlFor="name">Nome Completo</label>
-        <input type="text" id="name" />
-        {/* Sugestão de exibição de erro de validação */}
-        <div className="min-h-4">
-          <p className="text-xs text-red-400 mt-1">O nome é obrigatório.</p>
-        </div>
+        <input
+          type="text"
+          id="name"
+          {...register("name")}
+        />
+
+        {errors.email && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.name?.message as string}
+          </p>
+        )}
       </div>
       <div className="mb-4">
         <label htmlFor="email">E-mail</label>
-        <input className="" type="email" id="email" />
+        <input
+          className=""
+          type="email"
+          id="email"
+          {...register("email")}
+        />
+
+        {errors.email && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.email?.message as string}
+          </p>
+        )}
       </div>
       <div className="mb-4">
         <label htmlFor="password">Senha</label>
         <div className="relative">
-          <input type="password" id="password" />
+          <input
+            type={isPasswordVisible ? "text" : "password"}
+            id="password"
+            {...register("password")}
+          />
+          {errors.email && (
+            <p className="text-xs text-red-400 mt-1">
+              {errors.password?.message as string}
+            </p>
+          )}
+
           <span className="absolute right-3 top-3">
-            <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
-            {/* <EyeOffIcon
-                      className="text-slate-600 cursor-pointer"
-                      size={20}
-                    /> */}
+            <button
+              type="button"
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            >
+              {isPasswordVisible ? (
+                <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
+              ) : (
+                <EyeOffIcon
+                  size={20}
+                  className="text-slate-600 cursor-pointer"
+                />
+              )}
+            </button>
           </span>
         </div>
       </div>
       <div className="mb-4">
         <label htmlFor="confirm-password">Confirmar Senha</label>
         <div className="relative">
-          <input type="password" id="confirm-password" />
+          <input
+            type={isPasswordVisible ? "text" : "password"}
+            id="confirm-password"
+            {...register("password_confirmation")}
+          />
+          {errors.password_confirmation && (
+            <p className="text-xs text-red-400 mt-1">
+              {errors.password_confirmation?.message as string}
+            </p>
+          )}
+
           <span className="absolute right-3 top-3">
-            <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
-            {/* <EyeOffIcon
-                  className="text-slate-600 cursor-pointer"
+            <button
+              type="button"
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            >
+              {isPasswordVisible ? (
+                <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
+              ) : (
+                <EyeOffIcon
                   size={20}
-                /> */}
+                  className="text-slate-600 cursor-pointer"
+                />
+              )}
+            </button>
           </span>
         </div>
       </div>
       <div className="mb-4">
         <label htmlFor="phone">Telefone Celular</label>
-        <input type="text" id="phone" />
+        <input
+          type="text"
+          id="phone"
+          {...registerWithMask("phone", "(99) 99999-9999")}
+        />
+        {errors.phone && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.phone?.message as string}
+          </p>
+        )}
       </div>
       <div className="mb-4">
         <label htmlFor="cpf">CPF</label>
-        <input type="text" id="cpf" />
+        <input
+          type="text"
+          id="cpf"
+          {...registerWithMask("cpf", "999.999.999-99")}
+        />
+        {errors.cpf && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.cpf?.message as string}
+          </p>
+        )}
       </div>
       <div className="mb-4">
         <label htmlFor="cep">CEP</label>
-        <input type="text" id="cep" />
+        <input
+          type="text"
+          id="cep"
+          {...registerWithMask("zipcode", "99999-999")}
+          onBlur={handleZipcodeBlur}
+        />
+        {errors.zipcode && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.zipcode?.message as string}
+          </p>
+        )}
       </div>
       <div className="mb-4">
         <label htmlFor="address">Endereço</label>
@@ -61,6 +196,7 @@ export default function Form() {
           type="text"
           id="address"
           disabled
+          {...register("address")}
         />
       </div>
 
@@ -71,27 +207,39 @@ export default function Form() {
           type="text"
           id="city"
           disabled
+          {...register("city")}
         />
       </div>
       {/* terms and conditions input */}
       <div className="mb-4">
-        <input type="checkbox" id="terms" className="mr-2 accent-slate-500" />
+        <input
+          type="checkbox"
+          id="terms"
+          className="mr-2 accent-slate-500"
+          {...register("terms")}
+        />
         <label
           className="text-sm  font-light text-slate-500 mb-1 inline"
           htmlFor="terms"
         >
-          Aceito os{' '}
+          Aceito os{" "}
           <span className="underline hover:text-slate-900 cursor-pointer">
             termos e condições
           </span>
         </label>
+        {errors.terms && (
+          <p className="text-xs text-red-400 mt-1">
+            {errors.terms?.message as string}
+          </p>
+        )}
       </div>
 
       <button
         type="submit"
-        className="bg-slate-500 font-semibold text-white w-full rounded-xl p-4 mt-10 hover:bg-slate-600 transition-colors"
+        disabled={isSubmitting}
+        className="bg-slate-500 font-semibold text-white w-full rounded-xl p-4 mt-10 hover:bg-slate-600 transition-colors disabled:stroke-gray-200 flex justify-center"
       >
-        Cadastrar
+        {isSubmitting ? <Loader2 className="animate-spin" /> : "Cadastrar"}
       </button>
     </form>
   );
